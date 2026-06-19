7cc68431aef8282a8bc1991920144cbfe57a00e5
Cristian Marcus <cristianmarcus34@gmail.com>
2026-06-19 07:13:54 -0300

    Fix: wsgi.py apunta a settings (no a jardineria.settings)

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
-import os
+﻿import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jardineria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
-
-# 👇 AGREGÁ ESTA LÍNEA PARA VERCEL
 app = application
